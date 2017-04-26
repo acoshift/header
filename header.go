6fafc3092c47cb33587a922a918071174d0f3d25
acoshift <acoshift@gmail.com>
2017-04-27 00:05:55 +0700

    move headers from gzip

---
+++ header.go
@@ -0,0 +1,16 @@
+package header
+
+// Headers
+const (
+	AcceptEncoding  = "Accept-Encoding"
+	ContentType     = "Content-Type"
+	ContentEncoding = "Content-Encoding"
+	ContentLength   = "Content-Length"
+	SecWebSocketKey = "Sec-WebSocket-Key"
+	Vary            = "Vary"
+)
+
+// Encoding
+const (
+	Gzip = "gzip"
+)
